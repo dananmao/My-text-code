0c87e5fd2cb696e4906da8ddab0dc732a401bde1
dalanmao <zhaoliang1244@126.com>
2015-07-06 09:16:56 +0800

    Test git add

---
+++ myapplication/src/main/java/com/example/dalanmao/myapplication/utils/MyUtils.java
@@ -0,0 +1,10 @@
+package com.example.dalanmao.myapplication.utils;
+
+/**
+ * Created by Administrator on 2015/7/4.
+ */
+public class MyUtils {
+    public String getValue() {
+        return "No";
+    }
+}
